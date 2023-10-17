ae95895c2d383929fac167df15e2b63f06f0839d
Haky00 <haky.marek@gmail.com>
2023-10-18 00:22:22 +0200

    a

--- PhoneHandler.cs
+++ PhoneHandler.cs
@@ -22,6 +22,90 @@ public class DateTimeConverterUsingDateTimeParse : JsonConverter<DateTime>
 	}
 }
 
+public abstract class SensorHandler
+{
+	protected int i = 0;
+	protected double timestamp;
+	protected Node3D phone;
+	protected SensorData data;
+	public SensorHandler(Node3D phone, SensorData data)
+	{
+		this.phone = phone;
+		this.data = data;
+	}
+	public abstract void Update(double delta);
+}
+
+public class OrientationHandler : SensorHandler
+{
+	public OrientationHandler(Node3D phone, SensorData data) : base(phone, data)
+	{
+		if (data.RelativeOrientationSensor.Count > 0)
+		{
+			timestamp = data.RelativeOrientationSensor[0].T;
+		}
+	}
+
+	public override void Update(double delta)
+	{
+		if (i >= data.RelativeOrientationSensor.Count)
+		{
+			return;
+		}
+		if (timestamp >= data.RelativeOrientationSensor[i].T)
+		{
+			Quaternion quaternion = new(
+				(float)data.RelativeOrientationSensor[i].X,
+				(float)data.RelativeOrientationSensor[i].Y,
+				(float)data.RelativeOrientationSensor[i].Z,
+				(float)data.RelativeOrientationSensor[i].W
+			);
+			Vector3 rotation = quaternion.GetEuler(EulerOrder.Xyz);
+			phone.Rotation = rotation;
+			GD.Print(rotation);
+
+			// reset rotation
+			//Transform3D transform = phone.Transform;
+			//transform.Basis = Basis.Identity;
+			//phone.Transform = transform;
+
+			//phone.RotateObjectLocal(Vector3.Up, rotation.X); // first rotate about Y
+			//phone.RotateObjectLocal(Vector3.Right, rotation.Y); // then rotate about X
+			//phone.RotateObjectLocal(Vector3.Back, rotation.Z); // then rotate about Z
+			i++;
+		}
+		timestamp += delta * 1000;
+	}
+}
+
+public class AccelerationHandler : SensorHandler
+{
+	private Vector3 speed = new() { X = 0, Y = 0, Z = 0 };
+	public AccelerationHandler(Node3D phone, SensorData data) : base(phone, data)
+	{
+		if (data.LinearAccelerationSensor.Count > 0)
+		{
+			timestamp = data.LinearAccelerationSensor[0].T;
+		}
+	}
+	public override void Update(double delta)
+	{
+		if (i >= data.LinearAccelerationSensor.Count)
+		{
+			return;
+		}
+		if (timestamp >= data.LinearAccelerationSensor[i].T)
+		{
+			speed.X = (float)data.LinearAccelerationSensor[i].X;
+			speed.Y = (float)data.LinearAccelerationSensor[i].Y;
+			speed.Z = (float)data.LinearAccelerationSensor[i].Z;
+			i++;
+		}
+		phone.Translate(new(speed.X * (float)delta, speed.Y * (float)delta, speed.Z * (float)delta));
+		timestamp += delta * 1000;
+	}
+}
+
 public partial class PhoneHandler : Node3D
 {
 	// Called when the node enters the scene tree for the first time.
@@ -29,36 +113,22 @@ public partial class PhoneHandler : Node3D
 	{
 		JsonSerializerOptions options = new();
 		options.Converters.Add(new DateTimeConverterUsingDateTimeParse());
-		
+
 		string json = File.ReadAllText("data.json");
 		data = JsonSerializer.Deserialize<List<SensorData>>(json, options)[0];
-		timestamp = data.LinearAccelerationSensor[0].T;
+		sensorHandlers.Add(new OrientationHandler(this, data));
+		sensorHandlers.Add(new AccelerationHandler(this, data));
 	}
-
-	private int i = 0;
-	private double timestamp;
 	private SensorData data = new();
-    private Vector3 speed = new() { X = 0, Y = 0, Z = 0 };
+	private List<SensorHandler> sensorHandlers = new();
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (i >= data.LinearAccelerationSensor.Count)
+		foreach (SensorHandler handler in sensorHandlers)
 		{
-			return;
+			handler.Update(delta);
 		}
-
-		if (timestamp >= data.LinearAccelerationSensor[i].T)
-		{
-			speed.X = (float)data.LinearAccelerationSensor[i].X;
-            speed.Y = (float)data.LinearAccelerationSensor[i].Y;
-            speed.Z = (float)data.LinearAccelerationSensor[i].Z;
-            i++;
-			GD.Print(i);
-        }
-
-		Translate(new(speed.X * (float)delta, speed.Y * (float)delta, speed.Z * (float)delta));
-
-        timestamp += delta * 1000;
-    }
+	}
 }
